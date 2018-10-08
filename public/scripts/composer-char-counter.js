18487afcfd25e555219763d8de4d0ab35dcd5842
Sonchucks <son.hyun.uk@hotmail.com>
2018-10-08 23:36:46 +0000

    Set up basic page format along with tweet counter javascript

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var maxLength = 140;
+
+  $('textarea').on('input', function() {
+    var length = $(this).val().length;
+
+    length = maxLength - length;
+    $('.counter').text(length);
+
+    if (length < 0) {
+      $('.counter').css('color', 'red');
+    }
+  });
+});
+
